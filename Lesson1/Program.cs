5bb48c6b31a660244915944a276c0906dadf9192
busurman <alex.zakrevskiy@gmail.com>
2022-09-07 01:10:25 +0300

    Fourth task added

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -52,3 +52,18 @@ class Program {
 }
 */
 
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+class Program {
+  static void Main() {
+    int N;
+    Console.Write("N = ");
+    N = int.Parse(Console.ReadLine());
+    for(int i = 1; i < N; i++)
+    {
+        if(i%2 == 0)
+            Console.WriteLine(i+" ");
+    }
+  }
+}
+*/
