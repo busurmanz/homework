b270b99f9c9d07b41a16641731d1ecae13c1eeb5
busurman <alex.zakrevskiy@gmail.com>
2022-09-07 00:41:05 +0300

    First task added

---
+++ Lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+  static void Main() {
+    int a1, a2;
+    Console.Write("a1 = ");
+    a1 = int.Parse(Console.ReadLine());
+    Console.Write("a2 = ");
+    a2 = int.Parse(Console.ReadLine());
+    if(a1 > a2)
+        Console.Write("max = "+a1);
+    else
+        Console.Write("max = "+a2); 
+  }
+}
