37811f52e1c2eb3bd1e01b2bdd4c9b8f375398ec
busurman <alex.zakrevskiy@gmail.com>
2022-09-07 01:04:38 +0300

    Third task added

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -35,3 +35,20 @@ class Program {
   }
 }
 */
+
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+class Program {
+  static void Main() {
+    int a;
+    Console.Write("a = ");
+    a = int.Parse(Console.ReadLine());
+        if(a%2 == 0)
+            Console.WriteLine("even");
+        else
+            Console.WriteLine("odd");
+  }
+}
+*/
+
