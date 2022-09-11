a568b306d3b7bcad8dfe89c830c2dd78326044d9
busurman <alex.zakrevskiy@gmail.com>
2022-09-11 11:34:16 +0300

    task 1 added

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+class Program {
+  static void Main() {
+    int a1, a2, n1, n2, i;
+    Console.Write("a = ");
+    a1 = int.Parse(Console.ReadLine());
+    a2 = a1;
+    n1 = 0;
+    while(a2!=0){
+        n1++;
+        a2/=10;
+    }
+    int[] nums = new int[n1];
+    n2=10;
+    for(i = 0; i < n1-2; i++)
+        n2*=10;
+    for(i = 0; i < n1; i++){
+        nums[i] = a1/n2;
+        a1%=n2;
+        n2/=10;
+    }
+    Console.Write("num = "+nums[1]);
+  }
+}
+*/
+
+class Program {
+  static void Main() {
+    int a1, a2, n1, n2, i;
+    Console.Write("a = ");
+    a1 = int.Parse(Console.ReadLine());
+    a2 = a1;
+    n1 = 0;
+    while(a2!=0){
+        n1++;
+        a2/=10;
+    }
+    if(n1<3)
+        Console.WriteLine("no num");
+    else{
+        int[] nums = new int[n1];
+    n2=10;
+    for(i = 0; i < n1-2; i++)
+        n2*=10;
+    for(i = 0; i < n1; i++){
+        nums[i] = a1/n2;
+        a1%=n2;
+        n2/=10;
+    }
+    Console.Write("num = "+nums[2]);
+    }
+    
+  }
+}
