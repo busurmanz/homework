c3deafb5d0833c5a184935ae4395dce919df58f9
busurman <alex.zakrevskiy@gmail.com>
2022-09-25 19:45:07 +0300

    Task 1 added

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+ {
+static void Main() {
+    int x;
+    int schet = 0;
+    Console.WriteLine("Введите количество чисел ");
+    int count = Convert.ToInt32(Console.ReadLine());
+    int[] nums = new int[count];
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Введите число");
+        x = Convert.ToInt32(Console.ReadLine());
+        if(x > 0)
+        {
+            schet = schet + 1;
+        }
+    }
+    Console.WriteLine("Количество чисел больше 0 = " +schet);
+  }
+}
