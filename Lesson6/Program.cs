3c350fe014abbb2064be0148de1d044483350675
busurman <alex.zakrevskiy@gmail.com>
2022-09-25 20:12:08 +0300

    Some correction added

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -22,21 +22,22 @@ static void Main() {
 }
 */
 
+
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 class Program
  {
   static void Main() {
     Console.WriteLine();
-    Console.WriteLine("Введите коэффициент k1");
+    Console.WriteLine("Введите коэффициент k1 ");
     double k1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите коэффициент b1");
+    Console.WriteLine("Введите коэффициент b1 ");
     double b1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите коэффициент k2");
+    Console.WriteLine("Введите коэффициент k2 ");
     double k2 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите коэффициент b2");
+    Console.WriteLine("Введите коэффициент b2 ");
     double b2 = Convert.ToInt32(Console.ReadLine());
     double x = (b1 - b2) / (k2 - k1);
     double y = k1 * x + b1;
-    Console.WriteLine("Координаты точки перечения равны " +x+ " " +y);
+    Console.WriteLine("Координаты точки перечения равны " +x +" " +y);
   }
 }
