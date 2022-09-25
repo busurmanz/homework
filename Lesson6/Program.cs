3b5bf19acc507d91baf22bdb769f3df89de8fb12
busurman <alex.zakrevskiy@gmail.com>
2022-09-25 19:55:33 +0300

    Task 2 added

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿/*
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+class Program
  {
 static void Main() {
     int x;
@@ -16,5 +18,25 @@ static void Main() {
         }
     }
     Console.WriteLine("Количество чисел больше 0 = " +schet);
+  }
+}
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+class Program
+ {
+  static void Main() {
+    Console.WriteLine();
+    Console.WriteLine("Введите коэффициент k1");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент b1");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент k2");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите коэффициент b2");
+    double b2 = Convert.ToInt32(Console.ReadLine());
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k1 * x + b1;
+    Console.WriteLine("Координаты точки перечения равны " +x+ " " +y);
   }
 }
