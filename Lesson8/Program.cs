98a31abc7d2e405fc3c6443a696d36491a730044
busurman <alex.zakrevskiy@gmail.com>
2022-09-30 00:31:35 +0300

    some correction added

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -168,3 +168,47 @@ class Program
 }
 */
 
+class Program 
+{
+  static void Main() {
+    int[,] array = new int[4, 4];
+    int buf = 0;
+    Console.WriteLine();
+
+    for (int i = 0; i < 4; i++)
+    {
+            array[0, i] = buf;
+            buf += 1;
+    }
+    for (int i = 1; i < 4; i++)
+    {
+            array[i, 3] = buf;
+            buf += 1;
+    }
+    for (int i = 2; i > -1; i--)
+    {
+            array[3, i] = buf;
+            buf += 1;
+    }
+    for (int i = 2; i > 0; i--)
+    {
+            array[i, 0] = buf;
+            buf += 1;
+    }
+    for (int i = 1; i < 3; i++)
+    {
+            array[1, i] = buf;
+            buf += 1;
+    }
+    array[2, 2] = buf;
+    buf += 1;
+    array[2, 1] = buf;
+    
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+  }
+}
