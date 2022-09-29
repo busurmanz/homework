59011dbf111760b51941d9e54e0deb9ee9968d84
busurman <alex.zakrevskiy@gmail.com>
2022-09-29 11:08:06 +0300

    Task2 added

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -22,3 +22,40 @@ class Program
 }
 */
 
+/*
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+class Program
+ {
+  static void Main() {
+    int m = 8;
+    int n = 5;
+    int[,] array = new int[m, n];
+    
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = random.Next(100);
+        }
+    }
+    
+    Console.WriteLine("Введите номер строки элемента ");
+    int m1 = Convert.ToInt32(Console.ReadLine());
+    if ((m1 >= 0) && (m1 < m))
+    {
+        Console.WriteLine("Введите номер элемента в строке ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        if ((n1 >= 0) && (n1 < n))
+        {
+            Console.WriteLine("Элемент массива равен " + array[m1, n1]);
+        }
+        else
+            Console.WriteLine("Такого столбца нет в массиве ");
+    }
+    else
+        Console.WriteLine("Такой строки нет в массиве ");
+  }
+}
+*/
+
