f0ba192aecd426e2ff4f0547e3df5a2365d1568f
busurman <alex.zakrevskiy@gmail.com>
2022-10-01 22:30:37 +0300

    Task1 added

---
+++ Lesson9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+class Program 
+{
+  static void Main() {
+    Console.WriteLine("Введите начало числового отрезка ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите конец числового отрезка ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    for(int i = M; i < N; i++)
+    {
+        if(i > 0)
+            Console.WriteLine(i);
+    }
+  }
+}
