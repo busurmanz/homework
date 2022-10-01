11c053019a87f984f08f59eb7e5ec7d06e78d85d
busurman <alex.zakrevskiy@gmail.com>
2022-10-01 22:39:59 +0300

    Task2 added

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+﻿/*
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 class Program 
 {
   static void Main() {
@@ -12,5 +13,46 @@ class Program
         if(i > 0)
             Console.WriteLine(i);
     }
+  }
+}
+*/
+
+/*
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+class Program 
+{
+  static void Main() {
+    Console.WriteLine("Введите начало числового отрезка ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите конец числового отрезка ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int Sum = 0;
+    Console.WriteLine();
+    for(int i = M; i < N; i++)
+    {
+        if(i > 0)
+            Sum += i;
+    }
+    Console.WriteLine("Сумма натуральных элементов = " + Sum);
+  }
+}
+*/
+
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+class Program 
+{
+    static int A(int x, int y)
+    {
+        if (x == 0)
+            return y + 1;
+        else
+            if ((x != 0) && (y == 0))
+                return A(x - 1, 1);
+            else
+                return A(x - 1, A(x, y - 1));
+    }
+    static void Main() {
+        Console.WriteLine(A(2, 3));
   }
 }
