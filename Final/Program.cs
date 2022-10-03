20fcaeab1e8b019d11a9ee0d63be9bd86aba0aff
busurman <alex.zakrevskiy@gmail.com>
2022-10-03 18:05:59 +0300

    Some corrections added

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,24 +1,24 @@
 ﻿class Programm 
 {
   static void Main() {
-    string[] array1 = {"qaz", "wsx", "qwer", "asdf", "tgb", "a", "ik", "rfvbnm", "fff"}; // Задаем массив строк
-    string[] array2 = new string[array1.Length]; // Задаем пустой массив размером с изначальный
-    int count = 0; // Обнуляем счетчик
-    for(int i = 0; i < array1.Length; i++) // Перебираем все значения первого массива
+    string[] array1 = {"qaz", "wsx", "mvgdeuiu", "asdf", "tgb", "a", "ik", "gsfgdj", "fff"}; 
+    string[] array2 = new string[array1.Length]; 
+    int count = 0; 
+    for(int i = 0; i < array1.Length; i++) 
     {
-        if(array1[i].Length < 4) // Проверяем длинну подходящих строк
+        if(array1[i].Length < 4) 
         {
-            array2[count] = array1[i]; // Записываем подходящие строки в новый массив по порядку без пропусков
-            count += 1; // Обновляем счетчик, для устранения пустых ячеек в новом массиве
+            array2[count] = array1[i]; 
+            count += 1;
         }
     }
 
-    for(int i = 0; i < array2.Length; i++) // Перебираем новый массив
+    for(int i = 0; i < array2.Length; i++) 
     {
-        if(array2[i] != "") // Выводим элементы массива до момента пока не встретим пустую ячейку
+        if(array2[i] != " ") 
             Console.WriteLine(array2[i]);
         else
-            break; // Останавливаем цикл
+            break; 
     }
   }
 }
